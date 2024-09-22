efa20ef6fb765fed0b0e108be940e8782eacce97
Deus Work <deuswork@Deuss-MacBook-Air.local>
2024-09-22 21:51:05 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask the user for their name
+        Console.WriteLine("Hello! What is your name?");
+        
+        // Read the user's input
+        string name = Console.ReadLine();
+
+        // Greet the user by name
+        Console.WriteLine($"Nice to meet you, {name}!");
+    }
+}
