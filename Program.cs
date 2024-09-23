112a96c486d9bae243c62c054a10d86d520af505
DeusFlow <deuswork@icloud.com>
2024-09-23 20:39:10 +0200

    improved

--- Program.cs
+++ Program.cs
@@ -10,7 +10,30 @@ class Program
         // Read the user's input
         string name = Console.ReadLine();
 
-        // Greet the user by name
-        Console.WriteLine($"Nice to meet you, {name}!");
+        // Check if the user entered a name
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            // Inform the user that no name was entered
+            Console.WriteLine("It seems you didn't enter your name. Please try again.");
+        }
+        else
+        {
+            // Greet the user by name
+            Console.WriteLine($"Nice to meet you, {name}!");
+
+            // Check the length of the name and give feedback
+            if (name.Length < 5)
+            {
+                Console.WriteLine("You have a short and sweet name!");
+            }
+            else if (name.Length <= 10)
+            {
+                Console.WriteLine("Your name has a good length!");
+            }
+            else
+            {
+                Console.WriteLine("Wow, you have a long name!");
+            }
+        }
     }
 }
